ce799a3b868ff2605bd1d8b1894da01292b47d66
Umut Kalelioğlu <72888847+umutkaleli@users.noreply.github.com>
2026-01-15 03:23:40 +0300

    Feature: Caching Photos to the phone memory

--- app/src/main/java/com/example/contacts/ContactsApp.kt
+++ app/src/main/java/com/example/contacts/ContactsApp.kt
@@ -1,7 +1,29 @@
 package com.example.contacts
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class ContactsApp : Application()
+class ContactsApp : Application(), ImageLoaderFactory {
+
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.15) // Uses 15% of RAM for image caching
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(this.cacheDir.resolve("image_cache"))
+                    .maxSizePercent(0.02) // Uses 2% of disk space for offline situation
+                    .build()
+            }
+            .crossfade(true)
+            .build()
+    }
+}
